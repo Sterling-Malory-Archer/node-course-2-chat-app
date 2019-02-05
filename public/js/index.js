319ea4c88821ffec97fe2a7e4b18dcbd03ca12fe
Strahinja Dragojevic <sdragojevic@SPEL.LOCAL>
2019-02-05 14:16:41 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Sterling',
-        text: 'Yeah, that works for me'
-    });
 });
 
 socket.on('disconnect', function () {
